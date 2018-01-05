e42d081445c1fd4a0e9232d57967432d98511250
Antonio Pulido <rhema305@gmail.com>
2018-01-05 17:36:36 -0500

    added validations to the products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  class Product < ActiveRecord::Base
+  validates :description, :name, presence: true
+  validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
+  end
 end
