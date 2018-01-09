4b17adb19e130b8f4180de03a829aa497a0deb38
Antonio Pulido <rhema305@gmail.com>
2018-01-08 19:04:19 -0500

    added shopping cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,8 +2,8 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
 
-  def total_price
-    # returns the total price for the individual line item and entire cart,respectively
-    product.price * quantity
-  end
+  # def total_price
+  #   # returns the total price for the individual line item and entire cart,respectively
+  #   product.price * quantity
+  # end
 end
